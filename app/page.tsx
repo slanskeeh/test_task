d5229e4a3ecf4289d244f4320573ecca3729f969
Maxim Gerasimenko <maximgerasimenko@bk.ru>
2024-09-11 23:40:17 +0300

    ФИКС

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,14 @@ import Table from "@/widgets/Table/Table";
 
 export default async function Home() {
   const tableData = await fetch(
-    `${process.env.NEXT_PUBLIC_VERCEL_URL}/data/tableData.json`
-  ).then((res) => res.json());
+    `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/data/tableData.json`
+  )
+    .then((res) => res.json())
+    .catch((err) => console.log(err));
 
-  return <Main>{tableData?.table && <Table data={tableData.table} />}</Main>;
+  return (
+    <Main>
+      {tableData && tableData.table && <Table data={tableData.table} />}
+    </Main>
+  );
 }
