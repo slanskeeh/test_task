abb0fe9df05b367dcac82dc369a049ec4f4f6635
Maxim Gerasimenko <maximgerasimenko@bk.ru>
2024-09-11 23:51:13 +0300

    фикс запроса

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,47 @@ import { Main } from "@/shared/Main";
 import Table from "@/widgets/Table/Table";
 
 export default async function Home() {
-  const tableData = await fetch(
-    `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/data/tableData.json`
-  )
-    .then((res) => res.json())
-    .catch((err) => console.log(err));
-
-  console.log(
-    `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/data/tableData.json`
-  );
+  const tableData = [
+    {
+      id: 1,
+      name: "One",
+      icon: "/images/image.png",
+      params: "One, One",
+      visibility: true,
+    },
+    {
+      id: 2,
+      name: "Two",
+      icon: "/images/image.png",
+      params: "Two, Two",
+      visibility: true,
+    },
+    {
+      id: 3,
+      name: "Three",
+      icon: "/images/image.png",
+      params: "Three, Three",
+      visibility: true,
+    },
+    {
+      id: 4,
+      name: "Four",
+      icon: "/images/image.png",
+      params: "Four, Four",
+      visibility: true,
+    },
+    {
+      id: 5,
+      name: "Five",
+      icon: "/images/image.png",
+      params: "Five, Five",
+      visibility: true,
+    },
+  ];
 
   return (
     <Main>
-      {tableData && tableData.table && <Table data={tableData.table} />}
+      <Table data={tableData} />
     </Main>
   );
 }
