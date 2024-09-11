c2ea4e927ed3afe14b3938b460c9ea70cf5697e5
Maxim Gerasimenko <maximgerasimenko@bk.ru>
2024-09-11 23:34:08 +0300

    добавил env

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,8 @@ import Table from "@/widgets/Table/Table";
 
 export default async function Home() {
   const tableData = await fetch(
-    "http://localhost:3000/data/tableData.json"
+    `${process.env.NEXT_PUBLIC_VERCEL_URL}/data/tableData.json`
   ).then((res) => res.json());
 
-  console.log(tableData);
   return <Main>{tableData?.table && <Table data={tableData.table} />}</Main>;
 }
