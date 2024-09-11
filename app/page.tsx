0f6d62f9e55c2f6d911e1bf5b21b1ada142e32dd
Maxim Gerasimenko <maximgerasimenko@bk.ru>
2024-09-11 23:42:03 +0300

    fff

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,10 @@ export default async function Home() {
     .then((res) => res.json())
     .catch((err) => console.log(err));
 
+  console.log(
+    `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/data/tableData.json`
+  );
+
   return (
     <Main>
       {tableData && tableData.table && <Table data={tableData.table} />}
